e9be2ef4a7c11882335ca34ec563dc52a0ef67fb
rafi <rafiderberliner@googlemail.com>
2022-02-08 11:24:01 +0100

    Adds the change I submitted to the official repo.

--- Scene/tools/relation_test_net.py
+++ Scene/tools/relation_test_net.py
@@ -41,7 +41,6 @@ def main():
     )
 
     args = parser.parse_args()
-    print("args", args)
 
     num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
     distributed = num_gpus > 1
