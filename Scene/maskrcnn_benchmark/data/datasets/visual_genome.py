c6ad9cbca352446d945e4541c2b57c9f74d5ebde
rafi <rafiderberliner@googlemail.com>
2022-02-08 16:55:37 +0100

    Add tqdm

--- Scene/maskrcnn_benchmark/data/datasets/visual_genome.py
+++ Scene/maskrcnn_benchmark/data/datasets/visual_genome.py
@@ -116,14 +116,14 @@ class VGDataset(torch.utils.data.Dataset):
         self.custom_files = []
         self.img_info = []
         if os.path.isdir(path):
-            for file_name in os.listdir(path):
+            for file_name in tqdm(os.listdir(path)):
                 self.custom_files.append(os.path.join(path, file_name))
                 img = Image.open(os.path.join(path, file_name)).convert("RGB")
                 self.img_info.append({'width':int(img.width), 'height':int(img.height)})
         # Expecting a list of paths in a json file
         if os.path.isfile(path):
             file_list = json.load(open(path))
-            for file in file_list:
+            for file in tqdm(file_list):
                 self.custom_files.append(file)
                 img = Image.open(file).convert("RGB")
                 self.img_info.append({'width': int(img.width), 'height': int(img.height)})
